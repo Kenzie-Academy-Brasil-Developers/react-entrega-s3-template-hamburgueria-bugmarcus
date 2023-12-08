ceafffa7eb736af46805995df938c8c8fc528cc1
Marcus <bugmarcus@skeets.localdomain>
2023-12-08 10:48:04 -0300

    working on style

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -3,7 +3,7 @@ import styles from "./ProductCard.module.scss";
 export const ProductCard = ({ product }) => {
   return (
     <li className={styles.card}>
-      <img src={product.img} alt={product.name} />
+      <img className={styles.img} src={product.img} alt={product.name} />
       <div>
         <h3>{product.name}</h3>
         <span>{product.category}</span>
