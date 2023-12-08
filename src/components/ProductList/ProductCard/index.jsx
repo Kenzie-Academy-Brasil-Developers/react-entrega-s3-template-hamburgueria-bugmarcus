818a3e012a0fd42f928de6c146358dd210f4d0dc
Marcus <bugmarcus@skeets.localdomain>
2023-12-08 10:33:16 -0300

    Initial commit

--- src/components/ProductList/ProductCard/index.jsx
+++ src/components/ProductList/ProductCard/index.jsx
@@ -1,13 +1,20 @@
+import styles from "./ProductCard.module.scss";
+
 export const ProductCard = ({ product }) => {
-    return(
-        <li>
-            <img src={product.img} alt={product.name} />
-            <div>
-                <h3>{product.name}</h3>
-                <span>{product.category}</span>
-                <span>{product.price.toLocaleString('pt-BR', { style: "currency", currency: "BRL"})}</span>
-                <button>Adicionar</button>
-            </div>
-        </li>
-    )
-}
+  return (
+    <li className={styles.card}>
+      <img src={product.img} alt={product.name} />
+      <div>
+        <h3>{product.name}</h3>
+        <span>{product.category}</span>
+        <span>
+          {product.price.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </span>
+        <button>Adicionar</button>
+      </div>
+    </li>
+  );
+};
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,26 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CartModal } from "../../components/CartModal";
 import { Header } from "../../components/Header";
 import { ProductList } from "../../components/ProductList";
+import styles from "./HomePage.module.scss";
+import axios from "axios";
 
 export const HomePage = () => {
-   const [productList, setProductList] = useState([]);
-   const [cartList, setCartList] = useState([]);
+  const [productList, setProductList] = useState([]);
+  const [cartList, setCartList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://hamburgueria-kenzie-json-serve.herokuapp.com/products")
+      .then(function (response) {
+        setProductList(response.data);
+      });
+  }, []);
+  // useEffect montagem - carrega os produtos da API e joga em productList
+  // useEffect atualização - salva os produtos no localStorage (carregar no estado)
+  // adição, exclusão, e exclusão geral do carrinho
+  // renderizações condições e o estado para exibir ou não o carrinho
+  // filtro de busca
+  // estilizar tudo com sass de forma responsiva
 
-   // useEffect montagem - carrega os produtos da API e joga em productList
-   // useEffect atualização - salva os produtos no localStorage (carregar no estado)
-   // adição, exclusão, e exclusão geral do carrinho
-   // renderizações condições e o estado para exibir ou não o carrinho
-   // filtro de busca
-   // estilizar tudo com sass de forma responsiva
-
-   return (
-      <>
-         <Header />
-         <main>
-            <ProductList productList={productList} />
-            <CartModal cartList={cartList} />
-         </main>
-      </>
-   );
+  return (
+    <>
+      <Header />
+      <main>
+        <ProductList productList={productList} />
+        <CartModal cartList={cartList} />
+      </main>
+    </>
+  );
 };
