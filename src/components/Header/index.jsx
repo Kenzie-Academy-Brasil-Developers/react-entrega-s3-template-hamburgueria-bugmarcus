a6e99ec059093de01ce49ab53f5abcc5aae22ef3
Marcus Queiroz <marcusq@hotmail.com>
2024-02-06 16:13:25 -0300

    Fixed the modal

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Logo from "../../assets/Logo.svg";
 import { MdSearch, MdShoppingCart } from "react-icons/md";
-import { CartModal } from "../CartModal/index";
 
-export const Header = ({ cartList }) => {
-    const [value, setValue] = useState("");
-    const [showModal, setShowModal] = useState(false);
+export const Header = ({ cartList, setShowModal }) => {
+  const [value, setValue] = useState("");
 
-    return (
-        <>
-            <header>
-                <img src={Logo} alt="Logo Kenzie Burguer" />
-                <div>
-                    <button onClick={() => setShowModal(true)}>
-                        <MdShoppingCart size={21} />
-                        <span>0</span>
-                    </button>
-                    <form>
-                        <input
-                            type="text"
-                            value={value}
-                            onChange={(e) => setValue(e.target.value)}
-                        />
-                        <button type="submit">
-                            <MdSearch size={21} />
-                        </button>
-                    </form>
-                </div>
-            </header>
-            {showModal && <CartModal cartList={cartList} setShowModal={setShowModal} />}
-        </>
-    );
+  return (
+    <>
+      <header>
+        <img src={Logo} alt="Logo Kenzie Burguer" />
+        <div>
+          <button onClick={() => setShowModal(true)}>
+            <MdShoppingCart size={21} />
+            <span>0</span>
+          </button>
+          <form>
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit">
+              <MdSearch size={21} />
+            </button>
+          </form>
+        </div>
+      </header>
+    </>
+  );
 };
